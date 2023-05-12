b649ee7cac37bdc1a52d8711a6891c493b261cda
frontend-helper <108347339+frontend-helper@users.noreply.github.com>
2023-05-12 15:43:43 +0530

    Production class provider added.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,24 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "ca",
+});
 
 export default () => {
   return (
-    <BrowserRouter>
-      <div className="marketing-container">
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div className="marketing-container">
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 };
