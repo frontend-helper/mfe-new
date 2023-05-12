84983b366eda95ada74137157a9f8b91b9f2f74f
frontend-helper <108347339+frontend-helper@users.noreply.github.com>
2023-05-12 14:04:10 +0530

    Class name updated for marketing container.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
-    <div className="test">
+    <div className="marketing-container">
       <MarketingApp />
     </div>
   );
